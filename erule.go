7d059be82b24765ae48a3a9027cea4b059a624bd
Arturo Elias Anton <arturoeanton@gmail.com>
2019-06-18 19:05:19 -0300

    change by otto vm

--- erule.go
+++ erule.go
@@ -3,7 +3,7 @@ package erule
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/mattn/anko/vm"
+	"github.com/robertkrimen/otto"
 )
 
 type Politics struct {
@@ -17,20 +17,20 @@ type Rule struct {
 }
 
 const (
-	RISK    	int  = 0
-	FIRST_RULE  int  = 1
+	RISK       int = 0
+	FIRST_RULE int = 1
 )
 
 func Fire(str string, jsonString string) (int, []string, error) {
-	return fireMode(str, jsonString, RISK )
+	return fireMode(str, jsonString, RISK)
 }
 
 func FireFirstRule(str string, jsonString string) (string, error) {
-	_ , path , err := fireMode(str, jsonString, FIRST_RULE )
-	return  path[0], err
+	_, path, err := fireMode(str, jsonString, FIRST_RULE)
+	return path[0], err
 }
 
-func fireMode(str string, jsonString string, mode int ) (int, []string, error) {
+func fireMode(str string, jsonString string, mode int) (int, []string, error) {
 	p := Politics{}
 	if err := json.Unmarshal([]byte(str), &p); err != nil {
 		return 0, nil, fmt.Errorf("Execute error: %v\n", err)
@@ -41,25 +41,25 @@ func fireMode(str string, jsonString string, mode int ) (int, []string, error) {
 		return 0, nil, fmt.Errorf("Execute error: %v\n", err)
 	}
 
-	return fire(p, data, mode )
+	return fire(p, data, mode)
 }
 
 func fire(p Politics, data interface{}, mode int) (int, []string, error) {
 	risk := 0
 	var path []string
-	env := vm.NewEnv()
-	err := env.Define("data", data)
+	vm := otto.New()
+	err := vm.Set("data", data)
 	if err != nil {
 		return 0, nil, fmt.Errorf("Execute error: %v\n", err)
 	}
 	for _, rule := range p.Rules {
 		script := rule.Code
-		flag, err := env.Execute(script)
+		ret, err := vm.Run(script)
 		if err != nil {
 			return 0, nil, fmt.Errorf("Execute error: %v\n", err)
 		}
-		if flag.(bool) {
-
+		flag, _ := ret.ToBoolean()
+		if flag {
 			risk += rule.Point
 			path = append(path, rule.Name)
 			if mode == FIRST_RULE {
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/arturoeanton/erule
 
 go 1.12
 
-require github.com/mattn/anko v0.1.2
+require (
+	github.com/mattn/anko v0.1.2
+	github.com/robertkrimen/otto v0.0.0-20180617131154-15f95af6e78d
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
