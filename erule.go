23b42ba18756b4d4d300d4da6fd18c5e4ea1b98e
Arturo Elias Anton <arturoeanton@gmail.com>
2019-06-19 13:46:18 -0300

    change scope engine rule

--- erule.go
+++ erule.go
@@ -6,67 +6,113 @@ import (
 	"github.com/robertkrimen/otto"
 )
 
-type Politics struct {
-	Name  string `json:"name"`
-	Rules []Rule `json:"rules"`
-}
-type Rule struct {
-	Name  string `json:"name"`
-	Point int    `json:"point"`
-	Code  string `json:"code"`
-}
-
 const (
-	RISK       int = 0
-	FIRST_RULE int = 1
+	MODE_FIRST     string = "first_rule"
+	ERROR          string = "error"
+	OTTO_VALUE            = "otto.value"
+	RESPONSE_FIRST        = "erule.ResponseFirst"
+	RESPONSE_RISK         = "erule.ResponseRisk"
 )
 
-func Fire(str string, jsonString string) (int, []string, error) {
-	return fireMode(str, jsonString, RISK)
+type Politics struct {
+	Name           string `json:"name"`
+	Rules          []Rule `json:"rules"`
+	Mode           string `jons:"mode"`
+	After          string `json:"after"`
+	Before         string `json:"before"`
+	CustomResponse string `json:"custom_response"`
+}
+type Rule struct {
+	Name   string `json:"name"`
+	Point  int    `json:"point"`
+	Code   string `json:"code"`
+	After  string `json:"after"`
+	Before string `json:"before"`
 }
 
-func FireFirstRule(str string, jsonString string) (string, error) {
-	_, path, err := fireMode(str, jsonString, FIRST_RULE)
-	return path[0], err
+type ResponseFirst struct {
+	Value string
+}
+type ResponseRisk struct {
+	Value int
+	Path  []string
 }
 
-func fireMode(str string, jsonString string, mode int) (int, []string, error) {
+func Fire(str string, jsonString string, params ...interface{}) (string, interface{}, error) {
+	return fireMode(str, jsonString, params)
+}
+
+func fireMode(str string, jsonString string, params ...interface{}) (string, interface{}, error) {
 	p := Politics{}
 	if err := json.Unmarshal([]byte(str), &p); err != nil {
-		return 0, nil, fmt.Errorf("Execute error: %v\n", err)
+		return ERROR, nil, fmt.Errorf("Execute error: %v\n", err)
 	}
 	dataByt := []byte(jsonString)
 	var data map[string]interface{}
 	if err := json.Unmarshal(dataByt, &data); err != nil {
-		return 0, nil, fmt.Errorf("Execute error: %v\n", err)
+		return ERROR, nil, fmt.Errorf("Execute error: %v\n", err)
 	}
 
-	return fire(p, data, mode)
+	return fire(p, data, params)
 }
 
-func fire(p Politics, data interface{}, mode int) (int, []string, error) {
+func fire(p Politics, data interface{}, params ...interface{}) (string, interface{}, error) {
 	risk := 0
 	var path []string
 	vm := otto.New()
+
 	err := vm.Set("data", data)
 	if err != nil {
-		return 0, nil, fmt.Errorf("Execute error: %v\n", err)
+		return ERROR, nil, fmt.Errorf("Execute error(data): %v\n", err)
+	}
+
+	err = vm.Set("params", params)
+	if err != nil {
+		return ERROR, nil, fmt.Errorf("Execute error(params): %v \n", err)
+	}
+
+	if p.Before != "" {
+		vm.Run(p.Before)
 	}
 	for _, rule := range p.Rules {
+
+		if rule.Before != "" {
+			vm.Run(rule.Before)
+		}
+
 		script := rule.Code
 		ret, err := vm.Run(script)
 		if err != nil {
-			return 0, nil, fmt.Errorf("Execute error: %v\n", err)
+			return ERROR, nil, fmt.Errorf("Execute error: %v\n", err)
 		}
 		flag, _ := ret.ToBoolean()
+
+		if rule.After != "" {
+			vm.Run(rule.After)
+		}
+
 		if flag {
 			risk += rule.Point
 			path = append(path, rule.Name)
-			if mode == FIRST_RULE {
-				return 0, path, err
+			if p.Mode == MODE_FIRST {
+				break
 			}
 		}
 
 	}
-	return risk, path, nil
+	if p.After != "" {
+		vm.Run(p.After)
+	}
+
+	if p.CustomResponse != "" {
+		v, err := vm.Run(p.CustomResponse)
+
+		return OTTO_VALUE, v, err
+	}
+
+	if p.Mode == MODE_FIRST {
+		return RESPONSE_FIRST, ResponseFirst{Value: path[0]}, err
+	}
+
+	return RESPONSE_RISK, ResponseRisk{Value: risk, Path: path}, nil
 }
--- erule_test.go
+++ erule_test.go
@@ -2,6 +2,7 @@ package erule_test
 
 import (
 	"github.com/arturoeanton/erule"
+	"github.com/robertkrimen/otto"
 	"testing"
 )
 
@@ -14,33 +15,53 @@ func TestFire1(t *testing.T) {
 		{"name": "rule C", "point": 11, "code": "data.expo == 2"}
 		]
 	}`
-	risk, path, _ :=  erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
-	t.Log(path)
-	if risk != 11 {
-		t.Errorf("risk bad value")
-	}
-
-}
-
-
-func TestFire2(t *testing.T) {
-	p := `{ 
-	"name": "first example", 
-	"rules":[	 
-		{"name": "rule A", "point": 1, "code": "data.expo != 1"}, 
-		{"name": "rule B", "point": 10, "code": "data.ou == \"payroll\""},
-		{"name": "rule C", "point": 11, "code": "data.expo == 2"}
-		]
-	}`
-	risk, path, _ :=  erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
-	t.Log(path)
-	if risk != 10 {
+	_, r, _ := erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
+	t.Log(r.(erule.ResponseRisk).Path)
+	if r.(erule.ResponseRisk).Value != 11 {
 		t.Errorf("risk bad value")
 	}
 
 }
 
 func TestFire3(t *testing.T) {
+	p := `{ 
+	"name": "first example", 
+	"before": "console.log(\"p.Before\")",
+	"after": "console.log(\"p.After\")",
+	"custom_response": "s = {hola:\"chau\"}",
+	"rules":[	 
+		{"name": "rule A", "point": 1, "code": "data.expo != 1"}, 
+		{"name": "rule B", "point": 10, "code": "data.ou == \"payroll\""},
+		{"name": "rule C", "point": 11, "code": "data.expo == 2"}
+		]
+	}`
+	_, r, _ := erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
+	d, _ := r.(otto.Value).Object().Get("hola")
+	if d.String() != "chau" {
+		t.Errorf("risk bad value")
+	}
+
+}
+
+func TestFire4(t *testing.T) {
+	p := `{ 
+	"name": "first example", 
+	"before": "console.log(\"p.Before\")",
+	"after": "console.log(\"p.After\")",
+	"rules":[	 
+		{"name": "rule A", "point": 1, "code": "data.expo != 1"}, 
+		{"name": "rule B", "point": 10, "code": "data.ou == \"payroll\""},
+		{"name": "rule C", "point": 11, "code": "data.expo == 2"}
+		]
+	}`
+	_, r, _ := erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
+	if r.(erule.ResponseRisk).Value!= 10 {
+		t.Errorf("risk bad value")
+	}
+
+}
+
+func TestFire5(t *testing.T) {
 
 	p := `{ 
 	"name": "first example", 
@@ -50,28 +71,29 @@ func TestFire3(t *testing.T) {
 		]
 	}`
 
-	risk, path, _ :=  erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
-	t.Log(path)
-	if risk != 12 {
+	_, r, _ := erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
+
+	t.Log(r.(erule.ResponseRisk).Path)
+	t.Log(r.(erule.ResponseRisk).Path)
+	if r.(erule.ResponseRisk).Value != 12 {
 		t.Errorf("risk bad value")
 	}
 
 }
 
-
-func TestFire4(t *testing.T) {
+func TestFire6(t *testing.T) {
 
 	p := `{ 
-	"name": "first example", 
+	"name": "first example",
+	"mode": "first_rule",
 	"rules":[	 
 		{"name": "rule A", "point": 1, "code": "data.expo != 1 &&  data.ou == \"payroll\""}, 
 		{"name": "rule B", "point": 11, "code": "data.expo != 2"}
 		]
 	}`
 
-	path, _ :=  erule.FireFirstRule(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
-	t.Log(path)
-	if path != "rule B" {
+	_, r, _ := erule.Fire(p, `{"expo":1, "ou":"payroll","list":["a","b"]}`)
+	if r.(erule.ResponseFirst).Value != "rule B" {
 		t.Errorf("risk bad value")
 	}
 
